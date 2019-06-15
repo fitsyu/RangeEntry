5d3f167d70a4aaf8de80b0e36bbda932cb40eafa
fitsyu <fitsyu2@gmail.com>
2019-06-15 17:28:15 +0700

    implement RangeEntry minimally

---
+++ RangeEntry/Classes/RangeEntry.swift
@@ -0,0 +1,67 @@
+
+public enum RangeEntryState {
+    case OK
+    case InvalidRange
+    case Unknown(hint: String)
+}
+
+extension RangeEntryState: Equatable {}
+
+
+
+public class RangeEntry {
+    
+    public typealias Range = (start: Int, end: Int)
+    
+    public var state: RangeEntryState
+    
+    public var start: Int = 0 {
+        didSet {
+            checkStart()
+        }
+    }
+    
+    public var end: Int = 0 {
+        didSet {
+            checkEnd()
+        }
+    }
+    
+    public func range() -> Range {
+        return Range(start: start, end: end)
+    }
+    
+    public init() {
+        state = .OK
+        start = 0
+        end   = 0
+    }
+    
+    public init(start: Int, end: Int) {
+        state = .OK
+        
+        self.start = start
+        self.end   = end
+        
+        checkStart()
+        checkEnd()
+    }
+    
+    private func checkStart() {
+        if start < 0 { state = .InvalidRange }
+        
+        // the anti-pattern
+        if start > end {
+            end = start
+        }
+    }
+    
+    private func checkEnd() {
+        if end < 0 { state = .InvalidRange }
+        
+        // the anti-pattern
+        if end < start {
+            end = start
+        }
+    }
+}
